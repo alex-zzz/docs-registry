277d0afb9e9a930d407b806263e0cc93d97f6e0d
Дмитрий Гончар <dymych@outlook.com>
2017-07-07 20:01:07 +0300

    deserialize

--- DocsRegistry/Models/Customer.cs
+++ DocsRegistry/Models/Customer.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace DocsRegistry.Models
 {
+    [Serializable()]
+    [XmlType("Customer")]
+    [XmlRoot(ElementName = "Customer", Namespace = "http://www.TestBase.net")]
     public class Customer
     {
+        [XmlElement("Name")]
         public string Name { get; set; }
+        [XmlElement("EDRPOU")]
         public string EDRPOU { get; set; }
     }
 }
--- DocsRegistry/Models/Organization.cs
+++ DocsRegistry/Models/Organization.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace DocsRegistry.Models
 {
+    [Serializable()]
+    [XmlType("Organization")]
+    [XmlRoot(ElementName = "Organization", Namespace = "http://www.TestBase.net")]
     public class Organization
     {
+        [XmlElement("Name")]
         public string Name { get; set; }
+        [XmlElement("EDRPOU")]
         public string EDRPOU { get; set; }
     }
 }
